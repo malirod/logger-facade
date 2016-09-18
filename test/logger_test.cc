3a5c9548007ebfc3d61e3db73f301841ce4e7431
Rodion Malinovsky <rodionmalino@gmail.com>
2016-09-18 06:57:53 +0300

    Add stubs

---
+++ test/logger_test.cc
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(TestLogger, DummyTest) {
+    EXPECT_TRUE(true == true);
+}
---
+++ test/testrunner.cc
@@ -0,0 +1,5 @@
+#include "gtest/gmock.h"
+
+int main(int, char**) {
+    return RUN_ALL_TESTS();
+}
