418eadeeef05e053ff2e9594e87a961c13cd292e
Rodion Malinovsky <rodionmalino@gmail.com>
2016-09-28 06:33:04 +0300

    Fix formatting in the cpp file

--- test/logger_test.cc
+++ test/logger_test.cc
@@ -1,8 +1,7 @@
-#include "gtest/gtest.h"
 #include "logger.h"
+#include "gtest/gtest.h"
 
 TEST(TestLogger, DummyTest) {
-
   BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
   EXPECT_TRUE(true == true);
 }
