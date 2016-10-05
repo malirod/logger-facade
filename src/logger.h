db865615dd3a0c40898aa53d3ddef3846954b73f
Rodion Malinovsky <rodionmalino@gmail.com>
2016-10-05 06:53:48 +0300

    Add more macroses

--- src/logger.h
+++ src/logger.h
@@ -3,21 +3,92 @@
 #include <boost/log/core/core.hpp>
 #include <boost/log/trivial.hpp>
 
+#define LOGGER_EMPTY_BODY (void)0;
+
+#if !defined(DISABLE_LOGGER)
+
+// Strip off log lines lower or qeual DEBUG in DEBUG mode
+//#if !defined (DEBUG)
+//#define CUT_OFF_DEBUG_LOG
+//#endif // DEBUG
+
+#define LOGGER_CLASS_TYPE boost::log::trivial::logger_type
+
+#define LOGGER_SEVERITY_TYPE_TRACE boost::log::trivial::trace
+#define LOGGER_SEVERITY_TYPE_DEBUG boost::log::trivial::debug
+#define LOGGER_SEVERITY_TYPE_INFO boost::log::trivial::info
+#define LOGGER_SEVERITY_TYPE_WARN boost::log::trivial::warning
+#define LOGGER_SEVERITY_TYPE_ERROR boost::log::trivial::error
+#define LOGGER_SEVERITY_TYPE_FATAL boost::log::trivial::fatal
+
 namespace blsb {
 
-using LoggerType = boost::log::trivial::logger_type;
-
-void load_log_config();
+void load_log_config(const std::string& path);
 
 }  // namespace blsb
 
+#define INIT_LOGGER(LogConfigPath) blsb::load_log_config(LogConfigPath)
+
 #define BLSB_LOG(logger, severity) BOOST_LOG_SEV(logger, severity)
 
-#define LOG_TRACE(logger) BLSB_LOG(logger, boost::log::trivial::trace)
-#define LOG_INFO(logger) BLSB_LOG(logger, boost::log::trivial::info)
-#define LOG_DEBUG(logger) BLSB_LOG(logger, boost::log::trivial::debug)
-#define LOG_WARNING(logger) BLSB_LOG(logger, boost::log::trivial::warning)
-#define LOG_ERROR(logger) BLSB_LOG(logger, boost::log::trivial::error)
-#define LOG_FATAL(logger) BLSB_LOG(logger, boost::log::trivial::fatal)
+#if defined(CUT_OFF_DEBUG_LOG)
+#define LOG_TRACEL(logger) LOGGER_EMPTY_BODY
+#define LOG_DEBUGL(logger) LOGGER_EMPTY_BODY
+#else  // CUT_OFF_DEBUG_LOG
+#define LOG_TRACEL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_TRACE)
+#define LOG_DEBUGL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_DEBUG)
+#endif  // CUT_OFF_DEBUG_LOG
 
-#define INIT_LOGGER() blsb::load_log_config()
+#define LOG_INFOL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_INFO)
+#define LOG_WARNL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_WARN)
+#define LOG_ERRORL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_ERROR)
+#define LOG_FATALL(logger) BLSB_LOG(logger, LOGGER_SEVERITY_TYPE_FATAL)
+
+#define DECLARE_GET_LOGGER(LoggerName) \
+  LOGGER_CLASS_TYPE& GetLogger() {     \
+    static LOGGER_CLASS_TYPE logger;   \
+    return logger;                     \
+  }
+
+#define DECLARE_GLOBAL_GET_LOGGER(LoggerName) \
+  namespace {                                 \
+  DECLARE_GET_LOGGER(LoggerName)              \
+  }  // namespace
+
+#if defined(CUT_OFF_DEBUG_LOG)
+#define LOG_TRACE() LOGGER_EMPTY_BODY
+#define LOG_DEBUG() LOGGER_EMPTY_BODY
+#else  // CUT_OFF_DEBUG_LOG
+#define LOG_TRACE() LOG_TRACEL(GetLogger())
+#define LOG_DEBUG() LOG_DEBUGL(GetLogger())
+#endif  // CUT_OFF_DEBUG_LOG
+
+#define LOG_INFO() LOG_INFOL(GetLogger())
+#define LOG_WARN() LOG_WARNL(GetLogger())
+#define LOG_ERROR() LOG_ERRORL(GetLogger())
+#define LOG_FATAL() LOG_FATALL(GetLogger())
+
+#else  // DISABLE_LOGGER
+
+#define INIT_LOGGER(LogConfigPath) LOGGER_EMPTY_BODY
+
+#define BLSB_LOG(logger, severity) LOGGER_EMPTY_BODY
+
+#define LOG_TRACEL(logger) LOGGER_EMPTY_BODY
+#define LOG_DEBUGL(logger) LOGGER_EMPTY_BODY
+#define LOG_INFOL(logger) LOGGER_EMPTY_BODY
+#define LOG_WARNL(logger) LOGGER_EMPTY_BODY
+#define LOG_ERRORL(logger) LOGGER_EMPTY_BODY
+#define LOG_FATALL(logger) LOGGER_EMPTY_BODY
+
+#define DECLARE_GET_LOGGER(LoggerName) LOGGER_EMPTY_BODY
+#define DECLARE_GLOBAL_GET_LOGGER(LoggerName) LOGGER_EMPTY_BODY
+
+#define LOG_TRACE() LOGGER_EMPTY_BODY
+#define LOG_DEBUG() LOGGER_EMPTY_BODY
+#define LOG_INFO(l) LOGGER_EMPTY_BODY
+#define LOG_WARN(l) LOGGER_EMPTY_BODY
+#define LOG_ERROR() LOGGER_EMPTY_BODY
+#define LOG_FATAL() LOGGER_EMPTY_BODY
+
+#endif  // DISABLE_LOGGER
--- test/logger_test.cc
+++ test/logger_test.cc
@@ -1,8 +1,39 @@
 #include "logger.h"
 #include "gtest/gtest.h"
 
-TEST(TestLogger, DummyTest) {
-  blsb::LoggerType local_logger;
-  LOG_DEBUG(local_logger) << "lamer";
-  EXPECT_TRUE(true == true);
+DECLARE_GLOBAL_GET_LOGGER("LoggerTest.Global")
+
+namespace {
+
+void OutputTestLogLinesAnonymous() {
+  LOG_TRACE() << "Trace line";
+  LOG_DEBUG() << "Debug line";
+  LOG_INFO() << "Info line";
+  LOG_WARN() << "Warn line";
+  LOG_ERROR() << "Error line";
+  LOG_FATAL() << "Fatal line";
+}
+
+}  // namespace
+
+namespace Foo {
+
+class Bar {
+ public:
+  DECLARE_GET_LOGGER("Foo.Bar");
+
+  void OutputTestLogLines() {
+    OutputTestLogLinesAnonymous();
+  }
+};
+
+}  // namespace Foo
+
+TEST(TestLogger, LogFromFreeFunctionFromAnonymousNamespace) {
+  OutputTestLogLinesAnonymous();
+}
+
+TEST(TestLogger, LogFromClassMethod) {
+  Foo::Bar bar;
+  bar.OutputTestLogLines();
 }
