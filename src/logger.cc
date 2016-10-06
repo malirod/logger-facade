feef2cedab900c600a032bc28b9a8fe5ee0092c7
Alexandr Galiuzov <agaliuzov@luxoft.com>
2016-10-06 13:56:02 -0700

    Add both filterring and formating for Severity

--- src/logger.cc
+++ src/logger.cc
@@ -1,5 +1,6 @@
 #include "logger.h"
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/filter_parser.hpp>
 #include <boost/log/utility/setup/formatter_parser.hpp>
 #include <boost/log/utility/setup/from_stream.hpp>
 #include <fstream>
@@ -9,18 +10,16 @@ void blsb::init_logging(const std::string& config_file_path) {
   if (!log_config.is_open()) {
     return;
   }
-  boost::log::init_from_stream(log_config);
-
-  boost::log::add_common_attributes();
 
   boost::log::register_simple_formatter_factory<
       boost::log::trivial::severity_level,
       char>("Severity");
 
-  // boost::shared_ptr<boost::log::core> core = boost::log::core::get();
-  /*
-  core->add_global_attribute(
-    boost::log::keywords::severity,
-    boost::log::trivial::severity_level());
-  */
+  boost::log::register_simple_filter_factory<
+      boost::log::trivial::severity_level,
+      char>("Severity");
+
+  boost::log::init_from_stream(log_config);
+
+  boost::log::add_common_attributes();
 }
